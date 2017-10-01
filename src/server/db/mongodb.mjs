65bd956851e6116be3d848ff3b24f151e4dda1d4
otofune <otofune@gmail.com>
2017-10-02 05:42:57 +0900

    MongoDB接続エラーをハンドルする

--- src/server/db/mongodb.mjs
+++ src/server/db/mongodb.mjs
@@ -4,7 +4,14 @@ mongoose.Promise = global.Promise
 
 import config from '../../config'
 
-export default mongoose.createConnection(config.mongodb, {
+const db = mongoose.createConnection(config.mongodb, {
   useMongoClient: true,
   promiseLibrary: global.Promise
 })
+
+db.catch(e => {
+  console.error(e.stack)
+  process.exit(1)
+})
+
+export default db
